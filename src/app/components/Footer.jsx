c36e5fc3f12cd39c731b5556c2db385a3d22c3b9
Abhishek Rao <abhishek.komarraju@gmail.com>
2023-11-14 20:04:17 +0530

    Update Footer.jsx

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -1,10 +1,23 @@
 import React from "react";
+import Image from "next/image";
 
 const Footer = () => {
   return (
     <footer className="footer border z-10 border-t-[#33353F] border-l-transparent border-r-transparent text-white">
       <div className="container p-12 flex justify-between">
-        <span>LOGO</span>
+        <span className="flex text-2xl text-white font-semibold md:ml-8">
+          <Image
+            src={"/images/logo.png"}
+            width={45}
+            height={100}
+          />
+          <span className="pl-2 pt-1">
+            portfolio
+          </span>
+          <span className="pt-1 text-primary-400">
+            .
+          </span>
+        </span>
         <p className="text-slate-600">All rights reserved.</p>
       </div>
     </footer>
